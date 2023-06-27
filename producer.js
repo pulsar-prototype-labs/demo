8bb1ebb0cbb0574b29f217076bd27dc176a6fcd8
Grant <grantct71@gmail.com>
2023-06-26 17:21:36 -0700

    initial commit

---
+++ producer.js
@@ -0,0 +1,44 @@
+const Pulsar = require('pulsar-client');
+
+const produceMessage = async () => {
+  // Create a client
+  const client = new Pulsar.Client({
+    serviceUrl: 'pulsar://localhost:6650',
+  });
+
+  // Create a producer
+  const producer = await client.createProducer({
+    topic: 'my-topic',
+  });
+
+  // Send messages
+  for (let i = 0; i < 10; i += 1) {
+    const msg = `my-message-${i}`;
+    producer.send({
+      data: Buffer.from(msg),
+    });
+    console.log(`Sent message: ${msg}`);
+  }
+
+
+  // Interveral
+//   let count = 0;
+//   const interval = setInterval(() => {
+//     const msg = `my-message-${count}`;
+//     producer.send({
+//       data: Buffer.from(msg),
+//     });
+//     console.log(`Sent message: ${msg}`);
+//     count++;
+//   }, 1000)
+
+
+  await producer.flush();
+
+  await producer.close();
+  await client.close();
+};
+
+setInterval(() => {
+    produceMessage();
+}, 1000);
