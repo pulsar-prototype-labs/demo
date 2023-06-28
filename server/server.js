0d1650fa4f1218848cbfce7d4f746fa9751264e1
Grant <grantct71@gmail.com>
2023-06-28 14:47:07 -0700

    initial docker setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 4000;
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../index.html'))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`listening on Port ${PORT}`)
+});
